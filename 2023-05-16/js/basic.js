0869dce4d1ba0940111077536cec2714bea9f66f
LJH <95152341+LeeJinHyuk002@users.noreply.github.com>
2023-05-19 18:24:30 +0900

    Add files via upload

---
+++ 2023-05-16/js/basic.js
@@ -0,0 +1,8 @@
+$(function () {
+
+    $('.to_top').on('click', function () {
+
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    })
+
+})
---
+++ 2023-05-16/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.Slide').slick({
+        //arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+    });
+})
